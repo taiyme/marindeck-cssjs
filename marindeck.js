4aa1c5a00f38dc0274834296920efc93f8f51809
taiyme <53635909+taiyme@users.noreply.github.com>
2021-02-02 01:54:50 +0900

    update MarinDeck CSS/JS

--- marindeck.js
+++ marindeck.js
@@ -3,7 +3,7 @@
  * Copyright 2020 taiy https://github.com/taiyme
  * Apache License Version 2.0 https://www.apache.org/licenses/LICENSE-2.0
  * Forked from alwaysms (by @_phocom) https://greasyfork.org/ja/scripts/405029-alwaysms
- * Date: 2021-01-23 05:30 JST
+ * Date: 2021-02-02 01:30 JST
  */
 
 // 圧縮サイト => https://javascript-minifier.com/
@@ -14,19 +14,28 @@
   const deck = () => {
     const elms = document.querySelectorAll('time');
     for (const elm of elms) {
-      // if (!elm.classList.contains('js-timestamp')) continue;
-      elm.classList.remove('js-timestamp');
+      // if (!elm.classList.contains('js-timestamp')) continue;  // .js-timestampがなかったらスキップ
+      elm.classList.remove('js-timestamp');     // .js-timestampを消す(時刻上書き防止)
       const time = elm.querySelector('a');
       const span = elm.querySelector('span');
       if (time) {
-        const id = time.href.split('/')[5];
+        const id = time.href.split('/')[5];     // href属性からTweetIDを取得
         const set = formatDate(getDateFromSnowFrake(id));
         if (set) time.innerHTML = set;
       }
       if (span) {
-        const data = elm.dataset.time;
+        const data = elm.dataset.time;          // data-time属性からUnixTimeを取得
         const set = formatDate(new Date(Number(data)));
         if (set) span.innerHTML = set;
+      }
+    }
+    const details = document.querySelectorAll('.tweet-detail>.margin-tl.txt-mute.txt-size-variable--12');
+    for (const detail of details) {
+      const link = detail.querySelectorAll('a')[0];
+      if (link) {
+        const id = link.href.split('/')[5];     // href属性からTweetIDを取得
+        const set = formatDate(getDateFromSnowFrake(id), true);
+        if (set) link.innerHTML = set;
       }
     }
   }
@@ -36,21 +45,25 @@
     const unixTime = Math.floor(numid / 4194304) + 1288834974657;
     return new Date(unixTime);
   }
-  const formatDate = (date) => {
+  const formatDate = (date, isFull = false) => {
     if (Object.prototype.toString.call(date) !== '[object Date]') return false;
     const nowUnix  = new Date().getTime(),    // 現在の時刻
-          dateUnix = date.getTime();          // ツイートの時刻
-    const yyyy = date.getFullYear()     .toString().padStart(4, '0'),
-            MM = (date.getMonth()+1)    .toString().padStart(2, '0'),
-            dd = date.getDate()         .toString().padStart(2, '0'),
-            HH = date.getHours()        .toString().padStart(2, '0'),
-            mm = date.getMinutes()      .toString().padStart(2, '0'),
-            ss = date.getSeconds()      .toString().padStart(2, '0'),
-           SSS = date.getMilliseconds() .toString().padStart(3, '0');
-    if (nowUnix - dateUnix <= (3600000 * 2)){ // 3600000msec = 1時間 * nで好きな時間にできる
-      return `${HH}:${mm}:${ss}.${SSS}`       // 時:分:秒.ミリ秒
+          dateUnix = date.getTime(),          // ツイートの時刻
+          zeropad = (str, len) => str.toString().padStart(len, '0');
+    const yyyy = zeropad(date.getFullYear()    , 4),
+            MM = zeropad((date.getMonth()+1)   , 2),
+            dd = zeropad(date.getDate()        , 2),
+            HH = zeropad(date.getHours()       , 2),
+            mm = zeropad(date.getMinutes()     , 2),
+            ss = zeropad(date.getSeconds()     , 2),
+           SSS = zeropad(date.getMilliseconds(), 3);
+    if (isFull) {                             // ↓isFullがtrueのとき、年/月/日 時:分:秒.ミリ秒にする
+      return `${yyyy}/${MM}/${dd} ${HH}:${mm}:${ss}.${SSS}`;
     }
-    return `${yyyy}/${MM}/${dd}`              // その他の処理(いまは年/月/日)
+    if (nowUnix - dateUnix <= (3600000 * 2)) {// 3600000msec = 1時間 * nで好きな時間にできる
+      return `${HH}:${mm}:${ss}.${SSS}`;      // 時:分:秒.ミリ秒
+    }
+    return `${yyyy}/${MM}/${dd}`;             // その他の処理(いまは年/月/日)
   }
   setInterval(deck, 1000);
 })();
